07df9b4f7470a05ba53e1ec33187cec3a44148bb
Pat Morin <morin@scs.carleton.ca>
2014-05-21 16:33:54 -0400

    Almost ready to release

--- generate.py
+++ generate.py
@@ -14,18 +14,36 @@ def get_citation_stats():
 
 
 if __name__ == "__main__":
-    get_citation_stats()
+    """Generate a website from a collection of .htm files"""
+
+    # This file contains stuff common to all files.
     template = open("template.htm").read()
+
+    # Walk around looking for files ending in .html.
     for root, dirs, files in os.walk('.'):
         for p in [f for f in files if f.endswith('.htm')]:
             txt = open(os.path.join(root, p)).read()
             p = p[:-4]
+
+            # put this stuff into the template
             output = re.sub(r'\$content', txt, template)
             output = re.sub(r'\[\[{}\]\]'.format(p), p, output)
-            output = re.sub(r'\[\[(\w+)\]\]', 
-                            r'<a href="/\1.html">\1</a>', output)
+
+            # Substitute wiki-style [[ ]] links.
+            if root != '.':
+                subber =  r'<a href="/\1.html">\1</a>'
+            else:
+                subber =  r'<a href="\1.html">\1</a>'
+
+            # The first form is [[pagename]].
+            output = re.sub(r'\[\[(\w+)\]\]', subber, output)
+
+            # The second form is [[text|url]].
             output = re.sub(r'\[\[([\w ]+)\|([^\]]+)\]\]', 
                             r'<a href="\2">\1</a>', output)
+
+            # Write output .html file.
             fp = open(os.path.join(root, p) + ".html", "w")
             fp.write(output)
             fp.close()
+
